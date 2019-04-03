2aaa6f359fdd02132d46556ed17b7d518f11e353
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2019-04-03 11:53:32 +0200

    Boletin 27

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,14 @@
+
+package boletin27;
+
+/**
+ * @author raguiarperez
+ */
+public class Boletin27 {
+
+    public static void main(String[] args) {
+        Ventana cal=new Ventana();
+        cal.setVisible(true);
+    }
+    
+}
